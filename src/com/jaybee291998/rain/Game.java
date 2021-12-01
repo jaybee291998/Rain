877c5434dcd4c710cfd2358952f8401eca967246
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-12-01 09:58:03 +0800

    implemented Runnable interface for the game thread
    added threads for the game, and start and stop methods for the thread

--- src/com/jaybee291998/rain/Game.java
+++ src/com/jaybee291998/rain/Game.java
@@ -1,7 +1,22 @@
 package com.jaybee291998.rain;
 
-public class Game {
+public class Game implements Runnable {
 	public static int width = 300;
 	public static int heigth = width / 16 * 9;
 	public static int scale = 3;
+	
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
