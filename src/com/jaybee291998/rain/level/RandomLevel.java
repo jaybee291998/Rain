505bb6e5e2710977bc2dbfe281b528591c234d2c
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-05-02 15:35:53 +0800

    Update RandomLevel.java

--- src/com/jaybee291998/rain/level/RandomLevel.java
+++ src/com/jaybee291998/rain/level/RandomLevel.java
@@ -1,5 +1,20 @@
 package com.jaybee291998.rain.level;
 
-public class RandomLevel {
+import java.util.Random;
 
+public class RandomLevel extends Level{
+	
+	private final Random random = new Random();
+	
+	public RandomLevel(int x, int y) {
+		super(x, y);
+	}
+	
+	protected void generateLevel() {
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				tiles[x + y * width] = random.nextInt(8);
+			}
+		}
+	}
 }
