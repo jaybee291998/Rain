28f143defeb53885534207e94b14d93b3d53cebb
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-05-02 15:24:33 +0800

    added level class
    this class is in charge of managing a level in our game

---
+++ src/com/jaybee291998/rain/level/Level.java
@@ -0,0 +1,39 @@
+package com.jaybee291998.rain.level;
+
+import com.jaybee291998.rain.graphics.Screen;
+
+public class Level {
+	
+	private int width, height;
+	private int[] tiles;
+	
+	public Level(int width, int height) {
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		generateLevel();
+	}
+	
+	public Level(String path) {
+		loadLevel(path);
+	}
+
+	private void loadLevel(String path) {		
+	}
+
+	private void generateLevel() {
+	}
+	
+	public void update() {
+		
+	}
+	
+	private void time() {
+		
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen) {
+		
+	}
+
+}
