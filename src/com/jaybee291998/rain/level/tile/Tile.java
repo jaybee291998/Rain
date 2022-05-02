f0f14c235b65bd01024fff6b811011ae937f6e62
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-05-02 15:45:07 +0800

    Create Tile.java
    the tile class is in charge of rendering tiles on the screen based on its sprite

---
+++ src/com/jaybee291998/rain/level/tile/Tile.java
@@ -0,0 +1,22 @@
+package com.jaybee291998.rain.level.tile;
+
+import com.jaybee291998.rain.graphics.Screen;
+import com.jaybee291998.rain.graphics.Sprite;
+
+public class Tile {
+	
+	public int x, y;
+	public Sprite sprite;
+	
+	public  Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		
+	}
+	
+	public boolean solid() {
+		return false;
+	}
+}
