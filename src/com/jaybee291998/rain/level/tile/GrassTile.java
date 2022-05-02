294182b5d881892e0da2e63dcab711162a3da2b7
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-05-02 16:53:42 +0800

    Create GrassTile.java
    a grass tile

---
+++ src/com/jaybee291998/rain/level/tile/GrassTile.java
@@ -0,0 +1,15 @@
+package com.jaybee291998.rain.level.tile;
+
+import com.jaybee291998.rain.graphics.Screen;
+import com.jaybee291998.rain.graphics.Sprite;
+
+public class GrassTile extends Tile{
+	
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		
+	}
+}
