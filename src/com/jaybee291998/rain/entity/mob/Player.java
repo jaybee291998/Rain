5c1bdcb0f462258b79a8368e5cbf1d5152fc7f39
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-01 10:54:25 +0800

    added player class

---
+++ src/com/jaybee291998/rain/entity/mob/Player.java
@@ -0,0 +1,20 @@
+package com.jaybee291998.rain.entity.mob;
+
+public class Player extends Mob{
+	public Player() {
+		
+	}
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
+	}
+}
