e1bbc96ce415f14e6e002bd85d270d5e9896a22b
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-01 10:41:16 +0800

    added mob class
    a mob is an entity that can move on the map

---
+++ src/com/jaybee291998/rain/entity/mob/Mob.java
@@ -0,0 +1,22 @@
+package com.jaybee291998.rain.entity.mob;
+
+import com.jaybee291998.rain.entity.Entity;
+import com.jaybee291998.rain.graphics.Sprite;
+
+public class Mob extends Entity {
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+	
+	public void move() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	private boolean collision() {
+		return false;
+	}
+}
