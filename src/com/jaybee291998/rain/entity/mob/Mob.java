d8f71ac944b4f06c32ca27dd96ce7c2977997cbd
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-02 18:29:38 +0800

    implemented a more sophisticated collision detection

--- src/com/jaybee291998/rain/entity/mob/Mob.java
+++ src/com/jaybee291998/rain/entity/mob/Mob.java
@@ -2,6 +2,7 @@ package com.jaybee291998.rain.entity.mob;
 
 import com.jaybee291998.rain.entity.Entity;
 import com.jaybee291998.rain.graphics.Sprite;
+import com.jaybee291998.rain.level.tile.Tile;
 
 public class Mob extends Entity {
 	protected Sprite sprite;
@@ -9,25 +10,24 @@ public class Mob extends Entity {
 	protected boolean moving = false;
 	
 	public void move(int xa, int ya) {
+		if(xa != 0 && ya != 0) {
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
 		// 0 - north, 1 = east, 2 = south, 3 = west
 		//determine the direction of the player based on the movement
 		if(xa < 0) dir = 3;
 		if(xa > 0) dir = 1;
 		if(ya < 0) dir = 0;
 		if(ya > 0) dir = 2;
-		// check for collision
+		// check for collision on the x axis
 		if(!collision(xa, ya)) {
 			// move the mob
 			x += xa;
 			y += ya;
+			
 		}
-//		else {
-//			if(dir == 0) y += 1;
-//			if(dir == 2) y -= 1;
-//			if(dir == 1) x -= 1;
-//			if(dir == 3) x += 1;
-//			System.out.println("push back");
-//		}
 	}
 	
 	public void update() {
@@ -38,8 +38,27 @@ public class Mob extends Entity {
 		
 	}
 	
+//	private boolean collision(int xa, int ya) {
+//		int xPlus = (x + xa + 8)>>4;
+//		int xMinus = (x + xa - 8)>>4;
+//		int yPlus = (y + ya + 15)>>4;
+//		int yMinus = (y + ya - 15)>>4;
+//		
+//		Tile cornerTile1 = level.getTile(xMinus, yMinus);
+//		Tile cornerTile2 = level.getTile(xPlus, yMinus);
+//		Tile cornerTile3 = level.getTile(xPlus, yPlus);
+//		Tile cornerTile4 = level.getTile(xMinus, yPlus);
+//
+//		return cornerTile1.isSolid() || cornerTile2.isSolid() || cornerTile3.isSolid() || cornerTile4.isSolid();
+//	}
+
 	private boolean collision(int xa, int ya) {
-//		System.out.println("detect collison");
-		return level.getTile((x+xa)>>4,  (y+ya)>>4).isSolid();
+		boolean solid = false;
+		for(int c = 0; c < 4; c++) {
+			int xt = ((x + xa) + c%2 * 2 * 8 - 8) >> 4;
+			int yt = ((y + ya) + c/2 * 2 * 16 - 16) >> 4;
+			if(level.getTile(xt, yt).isSolid()) solid = true;
+		}
+		return solid;
 	}
 }
